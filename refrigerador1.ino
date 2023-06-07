c6d0e0204b85d280afe247042e51378b8cb2670f
Lívia Namba Seraphim <118857876+LivNS@users.noreply.github.com>
2023-06-07 00:50:53 -0300

    refrigerador1.ino

--- refrigerador1.ino
+++ refrigerador1.ino
@@ -4,13 +4,15 @@
 LiquidCrystal LCD(12,11,5,4,3,2);
 float valor_sen=0;
 float temperatura=0;
+
+//declarando
 void setup() {
   	pinMode(8, OUTPUT);	
 	pinMode(13, OUTPUT);
-  	pinMode(10, OUTPUT);//VENTILADOR IZQUIERDO 1
-  	pinMode(9, OUTPUT);//VENTILADOR IZQUIERDO 2
-  	pinMode(7, OUTPUT);//VENTILADOR DERECHO  1
-  	pinMode(6, OUTPUT);//VENTILADOR DERECHO  2
+  	pinMode(10, OUTPUT);
+  	pinMode(9, OUTPUT);
+  	pinMode(7, OUTPUT);
+  	pinMode(6, OUTPUT);
 	LCD.begin(16,2);
 	LCD.print("-Temperatura:");
 	LCD.setCursor(0,1);
@@ -19,16 +21,16 @@ void setup() {
 
 void loop() {
 	
-  valor_sen = analogRead(A0); //variable que almacenara los datos que tomara el sensor 
-  temperatura = ((((valor_sen*5.0)/1024)*1000)-500)/10; //esta formula nos permite la conversion de los datos a grados
+  valor_sen = analogRead(A0); 
+  temperatura = ((((valor_sen*5.0)/1024)*1000)-500)/10;
 
   	LCD.setCursor(0,1);
-	LCD.print(temperatura);//imprime temperatura en Celcuis
+	LCD.print(temperatura);
 	LCD.setCursor(9,1);
   
   	if (temperatura>=35){
   		digitalWrite(13, HIGH);
-  		digitalWrite(8,  LOW);//LED ROJO
+  		digitalWrite(8,  LOW);
       	digitalWrite(10, HIGH);
       	digitalWrite(9, HIGH);
       	digitalWrite(7, HIGH);
